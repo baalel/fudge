b7abb0a8895f8ae2e1cac16751cb06b584f37bc6
sprice <sprice@localhost.localdomain>
2013-01-28 13:17:11 +0000

    reworked as postservice that runs in seperate thread

--- src/uk/gov/tna/fudge/jExtractor/Solr/SolrPostman.java
+++ src/uk/gov/tna/fudge/jExtractor/Solr/SolrPostman.java
@@ -10,96 +10,153 @@ package uk.gov.tna.fudge.jExtractor.Solr;
  */
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.SolrInputDocument;
 
-public class SolrPostman {
+public class SolrPostman implements Runnable{
     
-    private String solrserver;
-    private static String charset = "UTF-8";
-    private String commit;
-    private String query;
-    private URLConnection urlConnection;
+    SolrServer server;
+    List<SolrServer> serverList;
+    boolean distributed;
+    private volatile Stack<List<SolrInputDocument>> workQueue;
+    List<SolrInputDocument> workingBatch;
+    private volatile boolean stopRequested = false;
+    private volatile boolean commit=false;
     
+    public SolrPostman(String solrServerUrl){
+        server = new HttpSolrServer(solrServerUrl);
+        distributed=false;
+        createQueue();
+    }
     
-    SolrPostman(String solrUrl){
-        try{
-            urlConnection=new URL(solrUrl).openConnection();
-            query="";
-            commit="";
-            this.solrserver=solrUrl;
-            this.solrserver="http://localhost:8983/solr/update";
-            try{
-                commit = URLEncoder.encode("commit=true", SolrPostman.charset);
-                query=String.format("param1=%s", commit);
-            }
-            catch(UnsupportedEncodingException e)
-            {
-                System.out.println("This shouldn't happen");
-            }
-            urlConnection.setUseCaches(false);
-            urlConnection.setDoOutput(true); // Triggers POST.
-            urlConnection.setRequestProperty("accept-charset", SolrPostman.charset);
-            urlConnection.setRequestProperty("content-type", "text/xml");
-
+    public SolrPostman(List<String> solrServerUrls){
+        serverList=new ArrayList<>(solrServerUrls.size());
+        server=new HttpSolrServer(solrServerUrls.get(0));
+        for(String url : solrServerUrls){
+            serverList.add(new HttpSolrServer(url));
         }
-        catch(IOException ioe){
-            System.out.println("Unable to open connection to solr" + ioe.getMessage());
-            ioe.printStackTrace();
+        distributed=true;
+        createQueue();
+    }
+    
+    private void createQueue(){
+        this.workQueue=new Stack<>();
+    }
+    
+    private void postDocument(List<SolrInputDocument> docs, boolean commit){
+       if(this.distributed){
+           distribute(docs, commit);
+           return;
+       }
+       try{
+            server.add(docs);
+            if(commit){
+                server.commit();
+            }
+        }
+        catch(SolrException | SolrServerException | IOException se){
+            System.out.println("Error posting test document");
+            System.out.println(se.getMessage());
             System.exit(1);
             
         }
+       
+   }
+    
+   private void distribute(List<SolrInputDocument> docs,boolean commit){
+       List<List<SolrInputDocument>> docLists=new ArrayList<>(serverList.size());
+       for(int i=0;i<serverList.size();i++){
+           docLists.add(new ArrayList<SolrInputDocument>(5000/serverList.size()+1));
+       }
+       for(SolrInputDocument doc : docs){
+           int s=Math.abs(((String)doc.getFieldValue("DREREFERENCE")).hashCode()%serverList.size());
+           if(s<0 || s>1){
+               System.out.println("ack");
+           }
+           docLists.get(s).add(doc);
+           
+       }
+       try{
+           for(int i=0;i<serverList.size();i++){
+                SolrServer s=serverList.get(i);
+                List<SolrInputDocument> l=docLists.get(i);
+                //serverList.get(i).add(docLists.get(i));
+                s.add(l);
+                if(commit){
+                    serverList.get(i).commit();
+                }
+            }
+        }
+        catch(SolrException se){
+            System.out.println("Solr Error posting document");
+            System.out.println(se.getMessage());
+            System.exit(1);
+            
+        }
+        catch(SolrServerException sse){
+            System.out.println("Server Error posting document\nCheck your firewall");
+            System.out.println(sse.getMessage());
+            System.exit(1);
+        }
+        catch(IOException ioe){
+            System.out.println("IO Error posting documents");
+            System.out.println(ioe.getMessage());
+            System.exit(1);
+            
+        }
+       catch(IllegalArgumentException e){
+           System.out.println("Failed to index to Solr\nCheck server spellings in config");
+           System.out.print(e.getMessage());
+           System.exit(1);
+       }
+       
+       
+    }
+   
+    public void requestStop() {
+        stopRequested = true;
+    }
+    
+    public void queueSolrPost(List<SolrInputDocument> docs, boolean commit){
+        workQueue.add(docs);
+        this.commit=commit;
+        
         
     }
-    public String post(String docs){
-            OutputStreamWriter writer = null;
-            try {
-                writer = new OutputStreamWriter(urlConnection.getOutputStream(), charset);
-                writer.write(query); // Write POST query string (if any needed).
-            } 
-            finally {
-                if (writer != null){
-                    try {
-                        writer.close();
-                    }
-                    catch (IOException logOrIgnore) {}
-                }
-                
-                
-                final char[] buffer=new char[8000];
-                final StringBuilder out = new StringBuilder();
-                try {
-                    InputStream result = urlConnection.getInputStream();
-                    final Reader in = new InputStreamReader(result, "UTF-8");
-                    try {
-                        for (;;) {
-                          int rsz = in.read(buffer, 0, buffer.length);
-                          if (rsz < 0)
-                            break;
-                          out.append(buffer, 0, rsz);
-                        }
-                    }
-                    finally {
-                      in.close();
+   
+    @Override
+    public void run(){
+        try {
+            while(!stopRequested || !workQueue.empty()){
+                if(!workQueue.empty()){
+                    System.out.println("Postman Posting, Queue is "+workQueue.size());
+                    workingBatch=workQueue.pop();
+                    this.postDocument(workingBatch, this.commit);
+                    if(this.commit){
+                        this.commit=false;
                     }
                 }
-                catch (UnsupportedEncodingException ex) {
-                  System.out.println("This shouldn't happen");
+                else{
+                    System.out.println("Postman Sleeping");
+                    Thread.sleep(10000L);
+
                 }
-                catch (IOException ioe) {
-                    System.out.println("Unable to open connection to solr" + ioe.getMessage());
-                    ioe.printStackTrace();
-                    System.exit(1);
-                }
-                return out.toString();
-                
             }
+        }
+        catch(InterruptedException iex) {}
     }
+    
+    public boolean hasMore(){
+        return !workQueue.empty();
+    }
+    
+    
+    
     
 }
